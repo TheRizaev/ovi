eec3f8e023d2613564114b75f0f30e5f3ddff2d8
Амир <158758261+TheRizaev@users.noreply.github.com>
2025-05-27 23:50:49 +0500

    Update settings.py

--- ovi/settings.py
+++ ovi/settings.py
@@ -57,10 +57,10 @@ WSGI_APPLICATION = 'ovi.wsgi.application'
 
 # Database configuration
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
-        conn_max_age=600,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
